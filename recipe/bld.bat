486bf42e41a34518cfa444985f13c11c3a72affc
Silvio Traversaro <silvio@traversaro.it>
2025-09-01 13:28:56 +0200

    Fix Windows and CUDA build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,17 @@
 mkdir build
 cd build
 
-cp %SRC_DIR%\src\win7\drivers\IntelRealSense_D400_series_win7.inf %SRC_DIR%
+:: I am not sure if this is actually required or not, but doing that fixes the build, see
+:: https://github.com/conda-forge/librealsense-feedstock/pull/76#issuecomment-3241165148 for more details
+copy %SRC_DIR%\src\win7\drivers\IntelRealSense_D400_series_win7.inf %SRC_DIR%
 
-cmake -GNinja ^
+if not "%cuda_compiler_version%" == "None" (
+  set "CMAKE_ARGS=%CMAKE_ARGS% -DBUILD_WITH_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES=all"
+) else (
+  set "CMAKE_ARGS=%CMAKE_ARGS% -DBUILD_WITH_CUDA=OFF"
+)
+
+cmake %CMAKE_ARGS% -GNinja ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -12,6 +20,10 @@ cmake -GNinja ^
     -DENABLE_CCACHE=OFF ^
     -DBUILD_WITH_OPENMP=OFF ^
     -DFORCE_RSUSB_BACKEND=ON ^
+    -DBUILD_PYTHON_BINDINGS=ON ^
+    -DPYTHON_INSTALL_DIR="%SP_DIR%" ^
+    -DPYTHON_EXECUTABLE="%PYTHON%" ^
+    -DPython_EXECUTABLE="%PYTHON%" ^
     -DBUILD_EXAMPLES=OFF ^
     -DBUILD_UNIT_TESTS=OFF ^
     -DLIBUSB_LIB=%LIBRARY_LIB%\libusb-1.0.lib ^
